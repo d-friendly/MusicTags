cd809e23ee65bcf4f90b2892ce55d59aa3b9235f
Dylan Friendly <friendly.dylan@gmail.com>
2021-10-19 14:42:30 -0500

    Created functionality for bottom navigation bar.

--- app/src/main/java/com/example/musictags/MainActivity.java
+++ app/src/main/java/com/example/musictags/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.musictags;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationBarView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnItemSelectedListener(bottomnavFunction);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
     }
+
+    private NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch(item.getItemId()) {
+                case R.id.home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.search:
+                    fragment= new SearchFragment();
+                    break;
+                case R.id.queue:
+                    fragment = new QueueFragment();
+                    break;
+                case R.id.settings:
+                    fragment = new SettingsFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+    };
 }
