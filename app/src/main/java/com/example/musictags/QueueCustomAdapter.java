3de4331b3b87ede49614db8176f9f4efbe425359
Dylan Friendly <friendly.dylan@gmail.com>
2021-10-25 13:24:13 -0500

    Created custom_queue_layout to create 'cards' for listview in QueueFragment.java
    Created CustomAdapter for the listview and custom layout.
    CustomAdapter handles the information for cards and has temporary test data entered.
    
    used thumbs up and thumbs down as icons to represent upvote and downvote.
    
    They are clickable but are not functional.

---
+++ app/src/main/java/com/example/musictags/QueueCustomAdapter.java
@@ -0,0 +1,82 @@
+package com.example.musictags;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class QueueCustomAdapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<String>();
+    private Context context;
+
+    public QueueCustomAdapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return 0;
+        //return list.get(pos).getId();
+        //just return 0 if your list items do not have an Id variable.
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.custom_queue_layout, null);
+        }
+
+        //Handle TextView and display string from your list
+        ImageView coverArt = (ImageView) view.findViewById(R.id.coverArt);
+        //TODO
+        //temporary data to show UI
+        coverArt.setImageResource(R.drawable.ic_home);
+        TextView tvSong= (TextView)view.findViewById(R.id.songTitle);
+        tvSong.setText("Song Title");
+        TextView tvArtist= (TextView)view.findViewById(R.id.artist);
+        tvArtist.setText("Artist");
+
+        //Handle buttons and add onClickListeners
+        ImageView upvotebtn= (ImageView)view.findViewById(R.id.upvote);
+        ImageView downvotebtn = (ImageView) view.findViewById(R.id.downvote);
+        upvotebtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //upvote song
+                //TODO
+            }
+        });
+
+        downvotebtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //downvote song and remove from queue
+                //TODO
+            }
+
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/musictags/QueueFragment.java
+++ app/src/main/java/com/example/musictags/QueueFragment.java
@@ -7,21 +7,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class QueueFragment extends Fragment {
 
 
+
+    private ArrayList<String> data;
     public QueueFragment() {
         // Required empty public constructor
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View v = inflater.inflate(R.layout.fragment_queue, container, false);
+        //add list to list view
+        ListView listView = (ListView) v.findViewById(R.id.listView);
+        //PLACHOLDER DATA
+        data = new ArrayList<String>(Arrays.asList("111,222,333,444,555,666".split(",")));
+        listView.setAdapter(new QueueCustomAdapter(data, getContext()) );
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_queue, container, false);
+        return v;
     }
 }
